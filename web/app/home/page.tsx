e9c526965b11212cf2d4104dd4f7c8c10171affe
FrancescoVenanti <francesco.venanti@gmail.com>
2024-08-20 15:23:10 +0200

    dependencies

---
+++ web/app/home/page.tsx
@@ -0,0 +1,30 @@
+
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  
+
+const Home = () => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+
+<Carousel>
+  <CarouselContent>
+    <CarouselItem>...</CarouselItem>
+    <CarouselItem>...</CarouselItem>
+    <CarouselItem>...</CarouselItem>
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel>
+
+        </div>
+    )};
+
+
+export default Home;
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -103,6 +104,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
+          <Link href={"/home"}>Home</Link>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
